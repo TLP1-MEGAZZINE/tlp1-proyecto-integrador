bd4c38b4d54683dbdf8a043a3219af5b1ea5490a
AgustinM12 <agustinmazza12345@gmail.com>
2023-06-11 14:10:09 -0300

    Se enlazaron todos los html creados hasta ahora, se unificaro los css en uno y se hicieron algunas correciones del contenido en los html 11/6/23

---
+++ js/login.js
@@ -0,0 +1,36 @@
+// Validación de nombre de usuario
+const usernameInput = document.getElementById('usernameInput');
+
+function validateUsername() {
+  const username = usernameInput.value.trim();
+
+  if (username === '') {
+    alert('Por favor, ingrese un nombre de usuario o correo válido.');
+    return false;
+  }
+
+  return true;
+}
+
+// Validación de contraseña
+const passwordInput = document.getElementById('passwordInput');
+
+function validatePassword() {
+  const password = passwordInput.value;
+
+  if (password.length < 8) {
+    alert('La contraseña debe tener al menos 8 caracteres.');
+    return false;
+  }
+
+  return true;
+}
+
+// Validación al hacer clic en el botón de enviar
+const submitButton = document.getElementById('submitButton');
+
+submitButton.addEventListener('click', function() {
+  if (!validateUsername() || !validatePassword()) {
+    event.preventDefault();
+  }
+});
---
+++ js/registro.js
@@ -0,0 +1,35 @@
+//input1
+const input = document.getElementById('miInput');
+  const mensaje = document.getElementById('mensaje');
+
+  input.addEventListener('focus', function() {
+    mensaje.style.display = 'inline';
+  });
+
+  input.addEventListener('blur', function() {
+    mensaje.style.display = 'none';
+  });
+  //input2
+  const input2 = document.getElementById('input2');
+  const mensaje2 = document.getElementById('mensaje2');
+
+  input2.addEventListener('focus', function() {
+    mensaje2.style.display = 'inline';
+  });
+
+  input2.addEventListener('blur', function() {
+    mensaje2.style.display = 'none';
+  });
+  //input3
+  const input3 = document.getElementById('input3');
+  const mensaje3 = document.getElementById('mensaje3');
+
+  input3.addEventListener('focus', function() {
+    mensaje3.style.display = 'inline';
+  });
+
+  input3.addEventListener('blur', function() {
+    mensaje3.style.display = 'none';
+  });
+
+
